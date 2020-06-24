048cfe217c1fc4f4a979ab378ff046ab9967cd2b
Mikolaj <mbo@ze-it.pl>
2020-06-24 21:46:58 +0200

    Safety - App create

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  StyleSheet
+} from 'react-native';
+
+const App: () => React$Node = () => {
+  return (
+    <>
+    
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
